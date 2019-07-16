33249c527da0184b8f8fbff3ea70b678d0d69911
Renato Balcazar <renatojbl99@gmail.com>
2019-07-15 19:37:58 -0500

    Ahori ya esta creado completamente jaja

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(require('./user.js'));
-app.use(require('./rol'));
-app.use(require('./sala'));
-app.use(require('./permiso_acceso'));
-app.use(require('./login'));
-module.exports = app;
